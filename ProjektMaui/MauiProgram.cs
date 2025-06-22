dc2a8adc69c33df7499f1b27a8a03233aa031f37
bszejko <biansz37@gmail.com>
2025-06-22 11:03:34 +0200

    zmiana czcionki

--- ProjektMaui/MauiProgram.cs
+++ ProjektMaui/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace ProjektMaui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                    fonts.AddFont("Roboto-SemiBold.ttf", "RobotoMedium");
                 });
 
 #if DEBUG
